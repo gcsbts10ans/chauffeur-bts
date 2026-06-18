b70bdfa71d8c496d5eeec7a55d0fb64f5c61efba
gcsbts10ans <blanchisserietoulousainedesant@gmail.com>
2026-06-18 13:03:38 +0200

    Add files via upload

--- sw-chauffeur.js
+++ sw-chauffeur.js
@@ -4,7 +4,7 @@
      (le cache ne sert que de secours hors-ligne)
    - Librairies CDN (Leaflet, Firebase) : cache d'abord (stables, versionnées)
    Incrémenter CACHE en cas de besoin de purge forcée. */
-const CACHE = 'chauffeur-v28';
+const CACHE = 'chauffeur-v29';
 const CDN = ['unpkg.com', 'www.gstatic.com'];
 
 self.addEventListener('install', e => {
